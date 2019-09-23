83d0c30baf4cb975cf3a2c6e0b429cfc522ad22f
Lutfi Ramadhan <lutfiramadan@gmail.com>
2019-09-23 22:23:39 +0700

    First Commit

---
+++ src/latihanjava/LatihanJava.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanjava;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lutfi
+ * NAMA  : LUTFI RAMADHAN
+ * NIM   : 10118908
+ * KELAS : IF 11 Karyawan
+ */
+public class LatihanJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR");
+        System.out.println("-----------------");
+        System.out.println("NAMA\t\t: Lutfi Ramadhan");
+        System.out.println("ALAMAT\t\t: Komplek Pharmindo");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 29 Desember 1998");
+        System.out.println("-----------------");
+        System.out.println("HOBI\t\t: Browsing");
+        System.out.println("MAKES\t\t: Kepiting");
+        System.out.println("MIKES\t\t: Chat Time");
+    }
+    
+}
